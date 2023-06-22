d2a2a700c226ff5b9117f7325e8c37ee67fce8bd
Kristina <kristina@Kristinas-MacBook-Pro.local>
2023-06-21 23:17:14 -0700

    cleaned server.js file

--- server.js
+++ server.js
@@ -7,19 +7,3 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// Connect to database
-// const db = mysql.createConnection(
-//   {
-//     host: 'localhost',
-//     port: 3306,
-//     // MySQL username,
-//     user: 'root',
-//     // TODO: Add MySQL password here
-//     password: 'BurtsB33s',
-//     database: 'business_db'
-//   },
-//   console.log(`Connected to the business_db database.`)
-// );
-
-// module.export = db;
